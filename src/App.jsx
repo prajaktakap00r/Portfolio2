0b4c28bd6dfd06505cb32ffc7529eb89f4a8258b
prajakta <89696648+prajaktakap00r@users.noreply.github.com>
2024-07-03 23:04:46 +0530

    404html

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import "./App.css";
 
 function App() {
   return (
-    <Router>
+    <Router basename="/portfolio">
       <div className="background">
         <NavBar />
         <div className="stars">
